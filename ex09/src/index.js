babf0d97e049d773a9de0011765613e703f05ec9
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-18 22:43:39 +0100

    Add files via upload

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,14 @@
+// Only change code below this line
+function myBouncer(arr) {
+var noviArr = [];
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == false) {
+        noviArr.push(arr[i]);
+    }
+}
+    return noviArr;
+}
+// Only change code above this line
 
+console.log(myBouncer([444, "", 0, false, null, 8, undefined, "", true, 0, false])); // Change this line
+module.exports = myBouncer;
