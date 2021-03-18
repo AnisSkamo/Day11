c12ac7da0a36f26e06e785235fecb3f6aec1853d
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-18 23:42:31 +0100

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,19 @@
+// Create a myPetsArray array here
+myPetsArray = ["Dog", "Cat"];
+// End of myPetsArray here
 
+function myArrayFunction(myPets) {
+    var myNewPets = [...myPets];
+    
+    // Only change code below this line
+    var myNewPets = myPetsArray();
+    myPetsArray.push("Bird", "Fish");
+    var firstPet = myPetsArray.shift ();
+    var lastPet = myPetsArray.pop ();
+    myPetsArray.unshift("Lion");
+
+    // Only change code above this line
+}
+
+console.log(myArrayFunction(myNewPets)); // Change this line
+module.exports = myArrayFunction;
