bc9e99d7ef4aa6629b146ecb8cfd91dcba0157d1
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-19 08:47:18 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,13 @@
+// Create a monitorsListArray array here
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+// End of monitorsListArray here
 
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+    // Only change code below this line
+    var monitorsList = [];
+    
+    // Only change code above this line
+}
+console.log(myMonitorsFunction()); // Change this line
+module.exports = myMonitorsFunction;
