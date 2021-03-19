3988e9c7eff903a87d0be585663cd5e7f333d755
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-19 08:48:25 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,14 @@
-
+// Only change code below this line
+var myArray = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+for (var i=0; i < myArray.length; i++) {
+    for (var j=0; j < myArray[i].length; j++) {
+        console.log(myArray[i][j]);
+    }
+}
+// Only change code above this line
+console.log(multiplyArrayFunction(), // Change this line
+module.exports = multiplyArrayFunction;
